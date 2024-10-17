6c2cfacc9895d2d02d1cfa738d5c35b98895ba49
Carlos Alexandre Pereira <carlosa.pereira@rio.rj.gov.br>
2024-10-17 09:11:46 -0300

    definindo css do input e do button

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import styles from './App.module.css';
 
 function App() {  
   return (
+    <>
     <header>
-    <div className={styles.headerBackground}>
-      <img src='../../public/Logo.svg' />
-    </div>  
-  </header>
+        <div className={styles.headerBackground}>
+          <img src='../../public/Logo.svg' />
+        </div>  
+    </header>
+    <div className={styles.containerNewTask}>
+      <input type='text' className={styles.inputNewTaks} />
+
+      <button className={styles.buttonNewTaks}>Criar</button>
+    </div>    
+   </>
   )
 }
 export default App
