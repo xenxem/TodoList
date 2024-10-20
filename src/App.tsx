c15beab51accdf20a8f3bdbefb3156efde53185a
Carlos Alexandre Pereira <carlosa.pereira@rio.rj.gov.br>
2024-10-20 17:29:07 -0300

    conclusão do desafio1

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,70 @@
 import './styles.css';
 import styles from './App.module.css';
 import {PlusCircle} from 'phosphor-react';
-import { ChangeEvent, Fragment, useState } from 'react';
+import { ChangeEvent, Fragment, useEffect, useState } from 'react';
 import { Task } from './components/Task';
 import { TaskEmpty } from './components/TaskEmpty';
+import {v4 as uuidv4} from 'uuid';
+
+
+export interface TarefaType {
+  id: string;
+  titulo: string;
+  concluida: boolean;
+}
 
 
 function App() {  
   
-  const [tarefasTotais,setTarefasTotais] = useState<number>(0);
+ 
   const [tarefasConcluidas,setTarefasConcluidas] = useState<number>(0);  
-  const [tarefa, setTarefa] = useState<string>('');
+  const [tarefa, setTarefa] = useState<TarefaType[]>([]);
+  const [tarefaDescricao, setTarefaDescricao] = useState<string>('');
 
- function handleNewTask() {            
-      setTarefasTotais(prevState => prevState +1);  
-      setTarefa('');
+  useEffect(()=>{
+    const tarefas = tarefa.filter(tarefa => tarefa.concluida === true);
+    setTarefasConcluidas(tarefas.length);
+  },[tarefa]);
+ 
+  const onClickStatus = (id: string,  concluida: boolean) => {        
+    setTarefa((prevState):TarefaType[] =>( 
+      prevState.map((tarefa) =>
+        tarefa.id === id ? {...tarefa, concluida: !concluida } : tarefa
+    )))        
+    finishedTasks();
   };
 
-  const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
-          const value = e.target.value;
-          setTarefa(value);
+  const deleteTask = (id:string)=>{
+    
+    const tarefas = tarefa.filter(tarefa => tarefa.id !== id);
+    setTarefa(tarefas);
+    setTarefasConcluidas(tarefas.length);       
   }
+
+  const finishedTasks = () => {
+      const tarefas = tarefa.filter(tarefa => tarefa.concluida === true);
+      setTarefasConcluidas(tarefas.length);       
+  } 
+
+ function handleNewTask() {    
+  const novaTarefa: TarefaType = {
+    id: uuidv4(), 
+    titulo: tarefaDescricao,
+    concluida: false};
+    setTarefa((prevState) => ([...prevState,novaTarefa]));      
+    setTarefaDescricao('');     
+ };
+
+ const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+          const value = e.target.value;
+          setTarefaDescricao(value);
+  }
+
 
 
   const reenderTaskInfo = () => {
 
-    if(tarefasTotais === 0){
+    if(tarefa.length === 0){
       return (
         <>
           <div className={styles.headerInfoTasks}>
@@ -41,11 +80,23 @@ function App() {
       return (
       <>
         <div className={styles.headerInfoTasks}>
-           <span>Tarefas criadas <span className={styles.completedTasks}>{tarefasTotais}</span></span>
-           <span className={styles.completedTaskText}>Concluídas <span className={styles.totalTasks}>{tarefasConcluidas} de {tarefasTotais}</span></span>
+           <span>Tarefas criadas <span className={styles.completedTasks}>{tarefa.length}</span></span>
+           <span className={styles.completedTaskText}>Concluídas <span className={styles.totalTasks}>{tarefasConcluidas} de {tarefa.length}</span></span>
         </div>
         <div className={styles.contentInfoTasks}>
-          <Task />                            
+          { 
+            tarefa.length > 0 && (
+              tarefa.map(t => 
+              <Task 
+                key={t.id}
+                id={t.id} 
+                titulo={t.titulo} 
+                concluida={t.concluida}
+                setTarefa={onClickStatus}
+                deleteTask={deleteTask}
+              />)
+            )
+          }
         </div>
       </>) 
     }
@@ -64,12 +115,12 @@ function App() {
         placeholder='Adicione uma nova tarefa'
         className={styles.inputNewTaks} 
         onChange={handleInputChange}        
-        value={tarefa}                
+        value={tarefaDescricao}                
         />
       <button 
         onClick={handleNewTask} 
-        className={ tarefa.length === 0 ? styles.buttonNewTaksDisabled : styles.buttonNewTaks}        
-        disabled={tarefa.length === 0 ? true : false}
+        className={ tarefaDescricao.length === 0 ? styles.buttonNewTaksDisabled : styles.buttonNewTaks}        
+        disabled={tarefaDescricao.length === 0 ? true : false}
         >Criar<PlusCircle size={24} />        
       </button>
     </div> 
